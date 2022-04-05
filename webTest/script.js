858e43b9b5713aee76260e5dac4a65d422e9bbeb
mickmick2110 <71765641+mickmick2110@users.noreply.github.com>
2022-04-05 13:22:56 +0200

    added more content

---
+++ webTest/script.js
@@ -0,0 +1,11 @@
+function hideDog(){
+
+    document.getElementById('doggo').style.display = 'none';
+
+}
+
+function showDog(){
+
+    document.getElementById('doggo').style.display = 'inline';
+    
+}
